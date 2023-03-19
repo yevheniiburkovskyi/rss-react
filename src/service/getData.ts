707545bae2aea423a6c9d3f5011be98afbc7ed41
Eugene <gradam2001@gmail.com>
2023-03-19 14:01:26 +0200

    refactor: add response type to fetch

--- src/service/getData.ts
+++ src/service/getData.ts
@@ -1,5 +1,5 @@
 export default async function getData() {
-  const res = await fetch('https://dummyjson.com/products?limit=30&skip=10')
+  const res: Response = await fetch('https://dummyjson.com/products?limit=30&skip=10')
     .then((res) => res.json())
     .catch(undefined);
   return res;
